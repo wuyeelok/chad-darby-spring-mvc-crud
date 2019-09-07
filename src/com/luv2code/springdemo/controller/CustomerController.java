ca6ba8b8f09088de873387aa75b74527120697a4
wuyeelok <wuyeelok@gmail.com>
2019-09-07 16:17:20 +0800

    Refactor: Use GetMapping instead requestmapping

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
@@ -21,7 +22,7 @@ public class CustomerController {
 		this.customerDAO = customerDAO;
 	}
 
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(ModelMap theModel) {
 
 		List<Customer> customers = this.customerDAO.getCustomers();
